636ceca55e78fc9c6865d0476dbb1fa0320a88ae
vicky seth <vickyseth9933@gmail.com>
2021-08-31 06:17:37 +0000

    first commit

--- first.bash
+++ first.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html
-touch index.html
+#touch index.html
 
 cat <<EOF  > index.html
-This is test file with script with jenkins demo
+This is test file with script with jenkins demo again 
 EOF
